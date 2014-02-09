d4896e8eae35237d8be1e8c89e42d93619a483d0
p1nox <pinox101@gmail.com>
2014-02-09 20:48:10 -0300

    sending maile after creating active admin user

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -4,7 +4,7 @@ class AdminUser < ActiveRecord::Base
   devise :database_authenticatable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  # after_create { |admin| admin.send_reset_password_instructions }
+  after_create { |admin| admin.send_reset_password_instructions }
 
   def password_required?
     new_record? ? false : super
