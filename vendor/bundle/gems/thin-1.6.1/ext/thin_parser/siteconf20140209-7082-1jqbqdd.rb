8d22507b848f3e046b4f1c657c07ce0a1c13cdb6
p1nox <pinox101@gmail.com>
2014-02-09 20:36:42 -0300

    deleting uneeded files

--- vendor/bundle/gems/thin-1.6.1/ext/thin_parser/siteconf20140209-7082-1jqbqdd.rb
+++
@@ -1,6 +0,0 @@
-require 'rbconfig'
-dest_path = "./.gem.20140209-7082-17ei50m"
-RbConfig::MAKEFILE_CONFIG['sitearchdir'] = dest_path
-RbConfig::CONFIG['sitearchdir'] = dest_path
-RbConfig::MAKEFILE_CONFIG['sitelibdir'] = dest_path
-RbConfig::CONFIG['sitelibdir'] = dest_path
