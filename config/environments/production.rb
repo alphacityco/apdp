f0e5567dc211fc13526f1853b4e9624860b8b22c
p1nox <pinox101@gmail.com>
2014-02-11 09:11:30 -0300

    configuration for variables in heroku, and sendgrid variables

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,7 +68,7 @@ Apdp::Application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = {:host => 'apdp-test.herokuapp.com'}
+  config.action_mailer.default_url_options = {:host => URI.parse(ENV['HOST_URL']).host}
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation can not be found).
